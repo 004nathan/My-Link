50e7d501a45fd01f3b6a5169cfc25f5772beee70
004nathan <parvathinathanbala@gmail.com>
2023-02-19 17:29:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,454 @@
+const data = [
+    {
+      "name": "Cliq",
+       "year": 2023,
+       "createdAt":"19-02-2023",
+     "catogery":"App",
+     "title":"cliq",
+       "url":"https://one.zoho.com/zohoone/zohocorp/home/cxapp/cliq/company/64396901/index.do",
+      "img":"assets/download.png"
+    },
+    {
+        "name": "Connect",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"connect",
+         "url":"https://connect.zoho.com/portal/zohoschools-2024/tags/frontend",
+        "img":"assets/download (2).png"
+      },
+      {
+        "name": "Show",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"show",
+         "url":"https://show.zoho.com/home/library/orglibrary#templates",
+        "img":"assets/download (3).png"
+      },
+      {
+        "name": "Mail",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"mail",
+         "url":"https://one.zoho.com/zohoone/zohocorp/home/cxapp/mail/zm/#mail/folder/inbox/p/1676716931640111693",
+        "img":"assets/download (4).png"
+      },
+      {
+        "name": "People",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"people",
+         "url":"https://one.zoho.com/zohoone/zohocorp/home/cxapp/people/zpeoplehr/zp#home/dashboard",
+        "img":"assets/download (5).png"
+      },
+       {
+      "name": "Workdrive",
+       "year": 2023,
+       "createdAt":"19-02-2023",
+     "catogery":"App",
+     "title":"workdrive",
+       "url":"https://one.zoho.com/zohoone/zohocorp/home/cxapp/workdrive/home/6oq3d00e1e07d58c64077b4f1c1f28da4a777/privatespace/sharedwithme/allusers/3yfnm18cd01a94c6941e38e2299b3f4c852d1?frameorigin=https%3A%2F%2Fone.zoho.com",
+      "img":"assets/download (6).png"
+    },
+    {
+        "name": "Sheets",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"sheets",
+         "url":"https://sheet.zoho.com/sheet/open/n6rdfba89cfea19814c1da2a146352fa23cde?sheet=November&range=D5",
+        "img":"assets/download (7).png"
+      },
+      {
+        "name": "Assignment",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"assignment",
+         "url":"https://table-layout.parvathi-nathan.repl.co/",
+        "img":"assets/download (8).png"
+      },
+      {
+        "name": "Writer",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"writer",
+        "url":"https://writer.zoho.com/writer/documents",
+        "img":"assets/download (9).png"
+      },
+      {
+        "name": "FontAwesome",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"fontawesome",
+        "url":"",
+        "img":"assets/download (10).png"
+      },
+      {
+        "name": "JavaTpoint",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"javatpoint",
+        "url":"https://www.javatpoint.com/java-string-replace",
+        "img":"assets/download (11).png"
+      },
+      {
+        "name": "Css Diner",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"cssdiner",
+        "url":"https://flukeout.github.io/",
+        "img":"assets/download (12).png"
+      },
+      {
+        "name": "Flexfroogy",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"flexfroogy",
+        "url":"https://flexboxfroggy.com/",
+        "img":"assets/images.png"
+      },
+      {
+        "name": "Binary",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"binary",
+        "url":"https://www.rapidtables.com/convert/number/decimal-to-hex.html?x=148",
+        "img":"assets/download.jpeg"
+      },
+      {
+        "name": "Codewar",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"codewar",
+        "url":"https://www.codewars.com/kata/592915cc1fad49252f000006",
+        "img":"assets/download (13).png"
+      },
+      {
+        "name": "Codepen",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"codepen",
+        "url":"https://codepen.io/your-work?search_term=null#_=_",
+        "img":"assets/download (14).png"
+      },
+      {
+        "name": "Coursera",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"coursera",
+        "url":"https://www.coursera.org/learn/java-programming/quiz/S5Ih4/using-storageresource?redirectToCover=true",
+        "img":"assets/download (15).png"
+      },
+      {
+        "name": "Grid",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"grid",
+        "url":"https://cssgridgarden.com/",
+        "img":"assets/download (16).png"
+      },
+      {
+        "name": "Replit",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"replit",
+        "url":"https://replit.com/@PARVATHI-NATHAN",
+        "img":"assets/download (17).png"
+      },
+      {
+        "name": "Creator",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"creator",
+        "url":"https://learn.zoho.com/portal/zohoschools/course/zs24-tech-fundamentals-of-rdbms/lesson/assignment-5-queries-on-world-database",
+        "img":"assets/download (18).png"
+      },
+      {
+        "name": "Udemy",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"udemy",
+        "url":"https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/25595388?start=15#overview",
+        "img":"assets/download (19).png"
+      },
+      {
+        "name": "W3Schools",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"w3schools",
+        "url":"https://www.w3schools.com",
+        "img":"assets/download (20).png"
+      },
+      {
+        "name":"Leaflet",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"leaflet",
+        "url":"https://leafletjs.com/examples.html",
+        "img":"assets/download (1).jpeg"
+      },
+      {
+        "name": "Npm",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"npm",
+        "url":"https://www.npmjs.com/package/body-parser",
+        "img":"assets/download (21).png"
+      },
+      {
+        "name": "Code beautifier",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"codebeautifier",
+        "url":"https://codebeautify.org/",
+        "img":"assets/download (22).png"
+      },
+      {
+        "name":"Angular",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"angular",
+        "url":"https://angular.io/guide/what-is-angular",
+        "img":"assets/download (23).png"
+      },
+      {
+        "name":"Jquery",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"jquery",
+        "url":"https://jqueryui.com/",
+        "img":"assets/download (24).png"
+      },
+      {
+        "name": "Api",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"api",
+        "url":"https://api-ninjas.com/api/babynames",
+        "img":"assets/download (2).jpeg"
+      },
+      {
+        "name": "Wordle",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"wordle",
+        "url":"https://www.nytimes.com/games/wordle/index.html",
+        "img":"assets/download (25).png"
+      },
+      {
+        "name": "Figma",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"figma",
+        "url":"https://www.figma.com",
+        "img":"assets/download (26).png"
+      },
+      {
+        "name": "Moral stories",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"moralstories",
+        "url":"https://www.moralstories.org/conversation-love-marriage/",
+        "img":"assets/download (27).png"
+      },
+      {
+        "name": "Bookfusion",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"bookfusion",
+        "url":"https://www.bookfusion.com/p/parvathinathan#_=_",
+        "img":"assets/download (3).jpeg"
+      },
+      {
+        "name": "Whatsapp",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"whatsapp",
+        "url":"https://web.whatsapp.com/",
+        "img":"assets/download (4).jpeg"
+      },
+      {
+        "name": "Deluge",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"deluge",
+        "url":"https://deluge.zoho.com/learndeluge#for-each-element",
+        "img":"assets/download (28).png"
+      },
+      {
+        "name": "Oracle",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"oracle",
+        "url":"https://www.oracle.com/in/java/technologies/downloads/tools/",
+        "img":"assets/download (29).png"
+      },
+      {
+        "name": "Scala",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"scala",
+        "url":"https://docs.scala-lang.org/getting-started/intellij-track/getting-started-with-scala-in-intellij.html#",
+        "img":"assets/download (30).png"
+      },
+      {
+        "name": "ChatGpt",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"chatgpt",
+        "url":"https://chat.openai.com/chat/686d644a-1c67-45ff-a32b-0c650ef16a05",
+        "img":"assets/download (31).png"
+      },
+      {
+        "name": "Instragam",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"instagram",
+        "url":"https://www.instagram.com/",
+        "img":"assets/download (5).jpeg"
+      },
+      {
+        "name": "Mysql",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"mysql",
+        "url":"https://dev.mysql.com/doc/refman/8.0/en/",
+        "img":"assets/download (32).png"
+      },
+      {
+        "name": "React",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"react",
+        "url":"https://reactjs.org/docs/getting-started.html",
+        "img":"assets/download (33).png"
+      },
+      {
+        "name": "Netlify",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"netlify",
+        "url":"https://app.netlify.com/teams/004nathan/overview",
+        "img":"assets/download (34).png"
+      },
+      {
+        "name": "Gmail",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"gmail",
+        "url":"https://mail.google.com/mail/u/0/#inbox/FMfcgzGrcjMkTnGzhghKKJHzSbcmLGCR",
+        "img":"assets/download (35).png"
+      },
+      {
+        "name": "Youtube",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"youtube",
+        "url":"https://www.youtube.com/",
+        "img":"assets/download (36).png"
+      },
+      {
+        "name": "PN car",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"pncar",
+        "url":"https://fantastic-fairy-774a27.netlify.app/",
+        "img":"assets/download (37).png"
+      },
+
+  ]
+// //   document.getElementById('logo').src=data[0].img;
+//  data.forEach(elem =>{
+//     console.log(elem.title);
+//  })
+const container = document.getElementsByClassName("content")[0];
+ const createApp = (data)=>{
+    data.forEach(elem=>{
+let parent = document.createElement('div');
+let title = document.createElement('a');
+let img = document.createElement('img');
+container.appendChild(parent).setAttribute('class','parent');
+parent.appendChild(img).setAttribute('class','img');
+parent.appendChild(title).setAttribute('class','title');
+title.innerHTML = elem.name;
+title.href=elem.url;
+title.target='blank';
+img.src=elem.img;
+    })
+ }
+ window.addEventListener('load',()=>{
+    createApp(data);
+ })
+ const getApp = ()=>{
+    let filter = [];
+    let text = document.getElementsByClassName("inputBoxSearch")[0].value;
+    text = text.toLowerCase();
+    for(let i of data){
+     if(i.title.indexOf(text) != -1){
+     filter.push(i);
+    }
+    else{
+    console.log('-1');
+    }
+    }
+   if( 1 <= filter.length){
+    console.log('some',filter)
+    container.innerHTML="";
+    createApp(filter);
+   }
+   else{
+    container.innerHTML="";
+    response();
+   }
+ }
+ const response = ()=>{
+    let send = data;
+    container.innerHTML=`
+    <div class="emoji">
+    <img src='assets/a8121abee959e18cbad25ad4046f76d8.gif'/>
+     <button onclick="home()">Home</button>
+   </div>`
+ }
+ const home = ()=>{
+    window.location.reload();
+ }
