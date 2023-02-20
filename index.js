fd53db653857644991cd88e227182f967e7169fe
004nathan <parvathinathanbala@gmail.com>
2023-02-20 09:49:19 +0530

    add translate

--- index.js
+++ index.js
@@ -395,6 +395,15 @@ const data = [
         "url":"https://fantastic-fairy-774a27.netlify.app/",
         "img":"assets/download (37).png"
       },
+      {
+        "name": "Translate",
+         "year": 2023,
+         "createdAt":"19-02-2023",
+       "catogery":"App",
+       "title":"translate",
+        "url":"https://www.google.com/search?q=google+translate&oq=google+tra&aqs=chrome.0.0i131i433i512j0i433i512j69i57j0i131i433i512l2j69i60j69i65j69i60.2880j0j7&sourceid=chrome&ie=UTF-8",
+        "img":"assets/download (38).png"
+      },
 
   ]
 // //   document.getElementById('logo').src=data[0].img;
