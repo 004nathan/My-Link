58f84feb241958ab216444ed7e4de5241b6447eb
004nathan <parvathinathanbala@gmail.com>
2023-04-01 20:01:22 +0530

    testing 2 for data length apply

--- index.js
+++ index.js
@@ -485,7 +485,42 @@ const data = [
         "url":"https://open.spotify.com",
         "img":"assets/download (46).png"
       },
-     
+      {
+        "name": "Visual Go",
+         "year": 2023,
+         "createdAt":"03-03-2023",
+       "catogery":"app",
+       "title":"visualgo",
+        "url":"https://visualgo.net/en/sorting?slide=1-1",
+        "img":"assets/download (47).png"
+      },
+      {
+        "name": "Typing",
+         "year": 2023,
+         "createdAt":"05-03-2023",
+       "catogery":"app",
+       "title":"typing",
+        "url":"https://www.typingclub.com/sportal/program-3/119.play",
+        "img":"assets/download (7).jpeg"
+      },
+      {
+        "name": "Notebook",
+         "year": 2023,
+         "createdAt":"01-04-2023",
+       "catogery":"app",
+       "title":"notebook",
+        "url":"https://notebook.zoho.com/",
+        "img":"assets/notebook.png"
+      },
+      {
+        "name": "Task",
+         "year": 2023,
+         "createdAt":"01-04-2023",
+       "catogery":"app",
+       "title":"task",
+        "url":"https://tasks.zoho.com/",
+        "img":"assets/task.png"
+      },
 
   ]
 // //   document.getElementById('logo').src=data[0].img;
